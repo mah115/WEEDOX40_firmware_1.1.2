728d880feb9b09f62109ad05f2ea90f7d0fd0ff6
Michael Hwang <>
2021-07-13 01:32:40 -0700

    https://github.com/mah115/WEEDOX40_firmware/issues/2 Fixed issue where M106 P0 also turned on P1 even if not in duplicate or mirror mode.

--- Marlin/src/gcode/temp/M106_M107.cpp
+++ Marlin/src/gcode/temp/M106_M107.cpp
@@ -65,9 +65,11 @@ void GcodeSuite::M106() {
     thermalManager.set_fan_speed(p, s);
 
     // perron add, 200806, 修正在复制和镜像模式右喷头风扇不转的问题
-    #if ENABLED(DUAL_X_CARRIAGE) && (dual_x_carriage_mode == DXC_DUPLICATION_MODE || dual_x_carriage_mode == DXC_MIRRORED_MODE)
-        if (p == 0)
-            thermalManager.set_fan_speed(1, s);
+    //mah115: this code sets both fans in mirror/duplicate mode, but we don't want this in regular mode
+    #if ENABLED(DUAL_X_CARRIAGE) 
+      if (dual_x_carriage_mode == DXC_DUPLICATION_MODE || dual_x_carriage_mode == DXC_MIRRORED_MODE){
+        if (p == 0) thermalManager.set_fan_speed(1, s);
+      }
     #endif
   }
 }
